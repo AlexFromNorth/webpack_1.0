7075f7d4fc3a0b0cc7407bfa64df6b3396ccdd42
AlexFromNorth_desc <jacklex.ab@gmail.com>
2024-05-29 14:17:19 +0300

    added dev server extensions

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,18 @@
 import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 type Mode = "production" | "development";
 
 interface EnvVariables {
   mode: Mode;
+  port: number;
 }
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === "development";
+
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
     entry: path.resolve(__dirname, "src", "index.ts"),
@@ -24,8 +28,8 @@ export default (env: EnvVariables) => {
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "public", "index.html"),
       }),
-      new webpack.ProgressPlugin(),
-    ],
+      isDev && new webpack.ProgressPlugin(),
+    ].filter(Boolean),
     module: {
       rules: [
         {
@@ -37,6 +41,17 @@ export default (env: EnvVariables) => {
     },
     resolve: {
       extensions: [".tsx", ".ts", ".js"],
+    },
+    devtool: isDev ? "inline-source-map" : false,
+    devServer: isDev
+      ? {
+          // static: "./dist",
+          port: env.port ?? 5000,
+          open: true,
+        }
+      : undefined,
+    optimization: {
+      runtimeChunk: "single",
     },
   };
   return config;
