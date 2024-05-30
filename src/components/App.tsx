5a0a768236ab8dfc76f6b2638352ba19ba034991
AlexFromNorth_desc <jacklex.ab@gmail.com>
2024-05-30 13:48:58 +0300

    added css and scss plugins and settings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './App.scss'
 
 export const App = () => {
   const [count, setCount] = useState(0)
@@ -8,8 +9,8 @@ export const App = () => {
   return (
     <div>
       <h1>Hello world</h1>
-      <button onClick={()=>{math(1)}}>knock +</button>
-      <button onClick={()=>{math(-1)}}>knock -</button>
+      <button onClick={()=>{math(1)}}><span>knock +</span></button>
+      <button onClick={()=>{math(-1)}}><span>knock -</span></button>
       <p>{count}</p>
     </div>
   )
