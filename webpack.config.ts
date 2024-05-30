8d0115f7ec2e7099dbc819e934d6e479156809be
AlexFromNorth_desc <jacklex.ab@gmail.com>
2024-05-30 16:07:53 +0300

    added restriturization with plugins bug

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,67 +1,25 @@
 import path from "path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-type Mode = "production" | "development";
+import { buildWebpack } from "./config/build/buildWebpack";
+import { BuildMode, BuildPaths } from "./config/build/types/types";
 
 interface EnvVariables {
-  mode: Mode;
+  mode: BuildMode;
   port: number;
 }
 
 export default (env: EnvVariables) => {
-  const isDev = env.mode === "development";
-
-  const config: webpack.Configuration = {
-    mode: env.mode ?? "development",
+  const paths: BuildPaths = {
+    output: path.resolve(__dirname, "build"),
     entry: path.resolve(__dirname, "src", "index.tsx"),
-    output: {
-      path: path.resolve(
-        __dirname,
-        env.mode == "development" ? "build" : "dist"
-      ),
-      filename: `[name].[contenthash].js`,
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public", "index.html"),
-      }),
-      isDev && new webpack.ProgressPlugin(),
-      new MiniCssExtractPlugin({
-        filename: "css/[name].[contenthash:8].css",
-        chunkFilename: "css/[name].[contenthash:8].css",
-      }),
-    ].filter(Boolean),
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-        },
-        {
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    devtool: isDev ? "inline-source-map" : false,
-    devServer: isDev
-      ? {
-          static: "./build",
-          port: env.port ?? 5000,
-          open: true,
-        }
-      : undefined,
-    optimization: {
-      runtimeChunk: "single",
-    },
+    html: path.resolve(__dirname, "public", "index.html"),
   };
+
+  const config: webpack.Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? "development",
+    paths,
+  });
   return config;
 };
